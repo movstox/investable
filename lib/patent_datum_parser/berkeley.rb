964eafbbe76e6f818212f4fbeb75bded1f5065dc
Aleksey Ivanov <mov.stox@gmail.com>
2015-11-09 18:26:43 +0200

    berkley: add 'suggested uses' as 'applications'

--- lib/patent_datum_parser/berkeley.rb
+++ lib/patent_datum_parser/berkeley.rb
@@ -2,4 +2,8 @@ class PatentDatumParser::Berkeley < PatentDatumParser::UCSF
   def abstract
     data = page.search('//h3[contains(., "Brief Description")][1]/following-sibling::node()[count(.|//h3[contains(., "Suggested uses")][1]/preceding-sibling::node()) = count(//h3[contains(., "Suggested uses")][1]/preceding-sibling::node())]').map {|x| x.text unless x.text.gsub(/\s/,'').empty?}.compact
   end
+
+  def applications
+    data = page.search('//h3[contains(., "Suggested uses")][1]/following-sibling::node()[count(.|//h3[contains(., "Advantages")][1]/preceding-sibling::node()) = count(//h3[contains(., "Advantages")][1]/preceding-sibling::node())]').map {|x| x.text unless x.text.gsub(/\s/,'').empty?}.compact
+  end
 end
