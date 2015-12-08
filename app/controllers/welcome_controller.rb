7d8c0a942cb0f681dbefc0fbb826b6ee3f7078f5
movstox <movstox@gmail.com>
2015-12-08 18:24:12 -0500

    basic results page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,81 @@ class WelcomeController < ApplicationController
   end
   
   def search
+    @bulk_data = [
+      {
+        id: 36232,
+        institution: 'stanford'
+      },
+      {
+        id: 31507,
+        institution: 'stanford'
+      },
+      {
+        id: 29491,
+        institution: 'stanford'
+      },
+      {
+        id: 28385,
+        institution: 'stanford'
+      },
+      {
+        id: 30055,
+        institution: 'stanford'
+      },
+      {
+        id: 25052,
+        institution: 'ucsf'
+      },
+      {
+        id: 25018,
+        institution: 'ucsf'
+      },
+      {
+        id: 24912,
+        institution: 'ucsf'
+      },
+      {
+        id: 24863,
+        institution: 'ucsf'
+      }
+    ]
     
+    @results = @bulk_data.map do |patent_to_find|
+      patent_to_find.merge(data: get_data(patent_to_find))
+    end
+  end
+
+  protected
+  def remap(arr)
+    {}.tap do |h|
+      arr.each do |el|
+        h[el[:id]] = el
+      end
+    end
+  end
+
+  def get_data(patent_to_find)
+    remap self.send('get_%s'%patent_to_find[:institution], patent_to_find[:id])
+  end
+
+  def get_berkley(id)
+    url = 'https://techtransfer.universityofcalifornia.edu/NCD/%s.html' % id
+    scrape ::PatentDatumParser::Berkeley, url
+  end
+
+  def get_ucsf(id)
+    url = 'https://techtransfer.universityofcalifornia.edu/NCD/%s.html' % id
+    scrape ::PatentDatumParser::UCSF, url
+  end
+
+  def get_stanford(id)
+    url = 'http://techfinder.stanford.edu/technology_detail.php'
+    vars = { ID: id } # patent id
+    scrape ::PatentDatumParser::Stanford, url, vars
+  end
+
+  def scrape(parser_class, url, params = {})
+    page = Mechanize.new.get(url, params)
+    parser_class.new(page: page).to_hash
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def has_data(v)
-    (v.kind_of?(Array) && v.any?) || (v.kind_of?(String) && v.present? && (v != 'N/A'))
+    (v.kind_of?(Array) && v.any?) || (v.kind_of?(String) && v.present? && (v != 'N/A')) || (v.kind_of?(Hash) && v.keys.any?)
   end
 
   def l(data, key)
