6823108c15840690816691a20bbbb8b7a9c7d5dd
movstox <movstox@gmail.com>
2015-12-14 20:42:05 -0500

    refactor existing controllers

--- app/controllers/patent/patent_datum_controller.rb
+++ app/controllers/patent/patent_datum_controller.rb
@@ -1,52 +1,36 @@
 class Patent::PatentDatumController < ApplicationController
   before_action :set_section_order
-  
+  before_action :init_scraper
+
   def berkeley
-    @p = remap get_berkley(params[:ref_id])
+    @p = remap @scraper.scrape_berkley(params[:ref_id])
     @institution = 'Berkeley'
     render 'patent_view'
   end
 
   def ucsf
-    @p = remap get_ucsf(params[:ref_id])
+    @p = remap @scraper.scrape_ucsf(params[:ref_id])
     @institution = 'UCSF'
     render 'patent_view'
   end
 
   def stanford
-    @p = remap get_stanford(params[:ref_id])
+    @p = remap @scraper.scrape_stanford(params[:ref_id])
     @institution = 'Stanford'
     render 'patent_view'
   end
 
   protected
+  def init_scraper
+    @scraper = ::PatentDatumScraper::Base.new
+  end
+
   def remap(arr)
     {}.tap do |h|
       arr.each do |el|
         h[el[:id]] = el
       end
     end
-  end
-
-  def get_berkley(id)
-    url = 'https://techtransfer.universityofcalifornia.edu/NCD/%s.html' % params[:ref_id]
-    scrape ::PatentDatumParser::Berkeley, url
-  end
-
-  def get_ucsf(id)
-    url = 'https://techtransfer.universityofcalifornia.edu/NCD/%s.html' % params[:ref_id]
-    scrape ::PatentDatumParser::UCSF, url
-  end
-
-  def get_stanford(id)
-    url = 'http://techfinder.stanford.edu/technology_detail.php'
-    vars = { ID: params[:ref_id] } # patent id
-    scrape ::PatentDatumParser::Stanford, url, vars
-  end
-
-  def scrape(parser_class, url, params = {})
-    page = Mechanize.new.get(url, params)
-    parser_class.new(page: page).to_hash
   end
 
   def set_section_order
--- app/controllers/scrape/patent_datum_controller.rb
+++ app/controllers/scrape/patent_datum_controller.rb
@@ -1,53 +1,38 @@
 class Scrape::PatentDatumController < ApplicationController
+  before_action :init_scraper
+
   def berkeley
-    @patent_datum = get_berkley params[:ref_id]
+    @patent_datum = @scraper.scrape_berkley params[:ref_id]
     render 'patent_datum'
   end
 
   def ucsf
-    @patent_datum = get_ucsf params[:ref_id]
+    @patent_datum = @scraper.scrape_ucsf params[:ref_id]
     render 'patent_datum'
   end
 
   def stanford
-    @patent_datum = get_stanford params[:ref_id]
+    @patent_datum = @scraper.scrape_stanford params[:ref_id]
     render 'patent_datum'
   end
 
   def view_berkeley
-    @patent_datum = get_berkley params[:ref_id]
+    @patent_datum = @scraper.scrape_berkley params[:ref_id]
     render 'patent_view'
   end
 
   def view_ucsf
-    @patent_datum = get_ucsf params[:ref_id]
+    @patent_datum = @scraper.scrape_ucsf params[:ref_id]
     render 'patent_view'
   end
 
   def view_stanford
-    @patent_datum = get_stanford params[:ref_id]
+    @patent_datum = @scraper.scrape_stanford params[:ref_id]
     render 'patent_view'
   end
 
   protected
-  def get_berkley(id)
-    url = 'https://techtransfer.universityofcalifornia.edu/NCD/%s.html' % params[:ref_id]
-    scrape ::PatentDatumParser::Berkeley, url
-  end
-
-  def get_ucsf(id)
-    url = 'https://techtransfer.universityofcalifornia.edu/NCD/%s.html' % params[:ref_id]
-    scrape ::PatentDatumParser::UCSF, url
-  end
-
-  def get_stanford(id)
-    url = 'http://techfinder.stanford.edu/technology_detail.php'
-    vars = { ID: params[:ref_id] } # patent id
-    scrape ::PatentDatumParser::Stanford, url, vars
-  end
-
-  def scrape(parser_class, url, params = {})
-    page = Mechanize.new.get(url, params)
-    parser_class.new(page: page).to_hash
+  def init_scraper
+    @scraper = ::PatentDatumScraper::Base.new
   end
 end
